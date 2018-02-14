d2fd007db2336183a18ee69d43df753677adb0f4
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-02-14 12:44:10 +0100

    Add JSX to contact and form

--- components/Contact.js
+++ components/Contact.js
@@ -10,10 +10,10 @@ var Contact = React.createClass({
           <img className={'contactImage'} src= {'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}>
         </div>
         <div className={'contactInfo'}>
-          <p className={'contactLabel'}> Imię: {this.props.item.firstName}</p>
+          <p className={'contactLabel'}>Imię: {this.props.item.firstName}</p>
           <p className={'contactLabel'}>Nazwisko: {this.props.item.lastName}</p>
           <a href={'mailto: ' + this.props.item.email}>{this.props.item.email}</a>
         </div>
       </div>
     )
-  }
+  });
