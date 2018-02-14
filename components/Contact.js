ba4eacbcc056cade89e521f0d2bfed2e830492f8
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-02-14 12:29:46 +0100

    Added JSX script to Contact

--- components/Contact.js
+++ components/Contact.js
@@ -5,18 +5,15 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      React.createElement('div', {className: 'contactItem'},
-        React.createElement('div', {className: 'contactImage'},
-          React.createElement('img', {
-            className: 'contactImage',
-            src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'})
-          ),
-        React.createElement('div', {className: 'contactInfo'},
-          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-          React.createElement('a', {className: 'contactEmail', href: 'mailto: ' + this.props.item.email}, this.props.item.email)
-        )
-      )
-    );
+      <div className={'contactItem'}>
+        <div className={'contactImage'}>
+          <img className={'contactImage'} src= {'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}>
+        </div>
+        <div className={'contactInfo'}>
+          <p className={'contactLabel'}> Imię: {this.props.item.firstName}</p>
+          <p className={'contactLabel'}>Nazwisko: {this.props.item.lastName}</p>
+          <a href={'mailto: ' + this.props.item.email}>{this.props.item.email}</a>
+        </div>
+      </div>
+    )
   }
-});
