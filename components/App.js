bd331fc4f47770f01c30a85c36ce050142fdf76c
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-02-19 19:55:14 +0100

    Fix brackets v3

--- components/App.js
+++ components/App.js
@@ -29,9 +29,9 @@ var contactForm = {
 var App = React.createClass({
   render: function() {
     return (
-      <div className= 'app'>
-        <ContactForm, contact={contactForm}>
-        <Contacts, items={contacts}>
+      <div className='app'>
+        <ContactForm contact={contactForm}>
+        <Contacts items={contacts}>
       </div>
       )
   }
