eacb3d23ace662d241615a53f54626b1ef5ca187
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-04-15 23:21:09 +0200

    Refactor code

--- components/App.js
+++ components/App.js
@@ -1,38 +1,52 @@
-var contacts = [
-  {
-    id: 1,
-    firstName: 'Jan',
-    lastName: 'Nowak',
-    email: 'jan.nowak@example.com',
-  },
-  {
-    id: 2,
-    firstName: 'Adam',
-    lastName: 'Kowalski',
-    email: 'adam.kowalski@example.com',
-  },
-  {
-    id: 3,
-    firstName: 'Zbigniew',
-    lastName: 'Koziol',
-    email: 'zbigniew.koziol@example.com',
-  }
-];
-
-var contactForm = {
-  firstName: '',
-  lastName: '',
-  email: ''
-};
+import React, { Component } from 'react';
+import styles from '../css/style.css';
+import ContactForm from './ContactForm';
+import Contacts from './Contacts';
 
 
-var App = React.createClass({
-  render: function() {
-    return (
-      <div className='app'>
-        <ContactForm contact={contactForm} />
-        <Contacts items={contacts} />
-      </div>
-      )
-  }
-});
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: [{
+                    id: 1,
+                    firstName: 'Jan',
+                    lastName: 'Nowak',
+                    email: 'jan.nowak@example.com',
+                },
+                {
+                    id: 2,
+                    firstName: 'Adam',
+                    lastName: 'Kowalski',
+                    email: 'adam.kowalski@example.com',
+                },
+                {
+                    id: 3,
+                    firstName: 'Zbigniew',
+                    lastName: 'Koziol',
+                    email: 'zbigniew.koziol@example.com',
+                }]
+            };
+        }
+
+    addContact(val) {
+        const contact = {
+            id: this.state.contacts.length + 1,
+            firstName: val.contacts.firstName,
+            lastName: val.contacts.lastName,
+            email: val.contacts.email
+        };
+
+        const contacts = [...this.state.contacts, contact];
+        this.setState({contacts});
+    }
+
+    render() {
+        return(
+            <div className='app'>
+                <ContactForm add={this.addContact} />
+                <Contacts items={contacts} />
+            </div>
+        );
+    }
+}
--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,16 +1,74 @@
-var ContactForm = React.createClass({
-  propTypes: {
-    contact: React.PropTypes.object.isRequired
-  },
+import React, { Component } from 'react';
+import style from '../css/style.css';
 
-  render: function() {
-    return (
-      <form className={'contactForm'}>
-        <input type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}></input>
-        <input type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}></input>
-        <input type={'email'} placeholder={'Email'} value={this.props.contact.email}></input>
-        <button type={'submit'}>Dodaj kontakt</button>
-      </form>
-    )
+
+class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [{
+        firstName: '',
+        lastName: '',
+        email: ''
+      }]
+    };
   }
-});
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.props.add(this.state.contacts);
+    this.setState({
+      contacts: [{
+        firstName: '',
+        lastName: '',
+        email: ''
+      }]
+    });
+  }
+
+  onFirstNameChange(e) {
+    e.preventDefault();
+    this.setState({
+      firstName: e.target.value
+    });
+  }
+
+  onNameChange(e) {
+    e.preventDefault();
+    this.setState({
+      lastName: e.target.value
+    });
+  }
+
+  onEmailChange(e) {
+    e.preventDefault();
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit} className={style.contactForm}>
+        <input type='text'
+               placeholder='Imię'
+               value={this.contact.firstName}
+               onFirstNameChange={this.updateFirstName}></input>
+        <input type='text'
+               placeholder='Nazwisko'
+               value={this.contact.lastName}
+               onNameChange={this.updateName}></input>
+        <input type={'email'}
+               placeholder='Email'
+               value={this.contact.email}
+               onEmailChange={this.updteMail}></input>
+        <button type='submit'>Dodaj kontakt</button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
